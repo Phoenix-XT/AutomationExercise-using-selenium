27bf3761f87a1e2b2c679ac03246f06f210e5b70
Nzan Christiana <omomnzan@gmail.com>
2024-06-08 14:59:15 +0100

    Update shopping.py

--- shopping.py
+++ shopping.py
@@ -3,6 +3,18 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Remote(
+    command_executor='http://localhost:4444/wd/hub',
+    options=options
+)
 
 # initializing the webdriver
 driver = webdriver.Chrome()
